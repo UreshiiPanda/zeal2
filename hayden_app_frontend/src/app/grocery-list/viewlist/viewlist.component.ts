db65941d7793c12f6be23dcc945870008a1ec36c
ianlee2024 <ian.lee@zealitconsultants.com>
2024-06-27 11:06:54 -0500

    updated grocery list and added packages

---
+++ hayden_app_frontend/src/app/grocery-list/viewlist/viewlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './viewlist.component.html',
+  styleUrl: './viewlist.component.css'
+})
+export class ViewlistComponent {
+
+}
